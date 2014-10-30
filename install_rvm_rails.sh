055c621b5c0470e8f0e1f578cf1a40fc6841378c
Richard <richard.be.jamin@gmail.com>
2014-10-30 00:46:59 -0500

    change

--- install_rvm_rails.sh
+++ install_rvm_rails.sh
@@ -1,5 +1,6 @@
 sudo apt-get update;
-sudo apt-get install nodejs
+sudo apt-get install nodejs;
+sudo apt-get install git-core;
 sudo apt-get install curl;
 curl -L get.rvm.io | bash -s stable;
 echo '';
